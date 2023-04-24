d7230119febe8c4fd5c5fa2e1fa368a6ea4f1a0b
TanjereeN98 <ahmadcyc35@gmail.com>
2023-04-24 15:41:55 -0700

    Updates_We_hate_ALX

--- new_print/AA_spec_printf.c
+++ new_print/AA_spec_printf.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * AA_spec_printf - function to choose the correct specifier format
- * @format: 
+ * @format: format received from the printf func
+ * @args: list of arguments passed
  * Return: 0 if success and -1 if '%' is the last char
  */
 
 int AA_spec_printf(const char *format, va_list args)
 {
-	int out_1, count, p_state = 0, printed_len = 0;
+	int p_state = 0, printed_len = 0;
 
 	while (format && *format)
 	{
@@ -19,7 +20,7 @@ int AA_spec_printf(const char *format, va_list args)
 			{
 				_putchar('%');
 				printed_len++;
-				format ++;
+				format++;
 			}
 			else if (*format == '%')
 				p_state = 1;
@@ -31,33 +32,8 @@ int AA_spec_printf(const char *format, va_list args)
 		}
 		else if (p_state == 1)
 		{
-			if (*format == 'c')
-			{
-				out_1 = va_arg(args, int);
-				_putchar(out_1);
-				printed_len++;
-			}
-			else if (*format == 's')
-			{
-				const char *s = va_arg(args, const char *);
-				while (*s)
-				{
-					_putchar(*s++);
-					printed_len++;
-				}
-			}
-			else if(*format == 'd' || *format == 'i')
-			{
-				int i = va_arg(args, int);
-				char AA_array[32];
-				AA_itwos(i, AA_array);
-				for (count = 0 ; AA_array[count] != '\0' ; count ++)
-				{
-					_putchar(AA_array[count]);
-					printed_len++;
-				}
-			}
-			p_state = 0;
+			AA_spec_mode(format, args, printed_len);
+				p_state = 0;
 		}
 		format++;
 	}
--- new_print/main.h
+++ new_print/main.h
@@ -6,5 +6,6 @@
 int _putchar(char c);
 void AA_itwos(int num, char *s_buff);
 int AA_spec_printf(const char *format, va_list args);
+int AA_spec_mode(const char *format, va_list args, int printed_len);
 int _printf(const char *format, ...);
 #endif
