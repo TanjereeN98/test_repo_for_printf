1c61917d524a28c06522db1929261cd7304350dd
TanjereeN98 <ahmadcyc35@gmail.com>
2023-04-24 16:59:01 -0700

    Vanilla Update

--- new_print/main.c
+++ new_print/main.c
@@ -9,8 +9,8 @@ int main(void)
     len2 = printf("Let's try to printf a simple sentence.\n");
     _printf("Length:[%d, %i, %c, %i]\n", len, len, 'H', -10);
     printf("Length:[%d, %i, %c, %i]\n", len2, len2, 'H', -10);
-    _printf("Negative:[%d]\n%", -762534);
-    printf("Negative:[%d]\n%", -762534);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
--- new_print/main.h
+++ new_print/main.h
@@ -8,5 +8,6 @@ int _putchar(char c);
 void AA_itwos(int num, char *s_buff);
 int AA_spec_printf(const char *format, va_list args);
 int AA_spec_mode(const char *format, va_list args, int printed_len);
+char *_memset(char *s, char b, unsigned int n);
 int _printf(const char *format, ...);
 #endif
