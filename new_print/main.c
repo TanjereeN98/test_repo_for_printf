6bde8b9911d47be9deb3f59b835be243537eb81c
TanjereeN98 <ahmadcyc35@gmail.com>
2023-04-24 16:30:46 -0700

    Update

--- new_print/main.c
+++ new_print/main.c
@@ -7,10 +7,10 @@ int main(void)
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
+    _printf("Length:[%d, %i, %c, %i]\n", len, len, 'H', -10);
+    printf("Length:[%d, %i, %c, %i]\n", len2, len2, 'H', -10);
+    _printf("Negative:[%d]\n%", -762534);
+    printf("Negative:[%d]\n%", -762534);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
--- new_print/main.h
+++ new_print/main.h
@@ -2,6 +2,7 @@
 #define AA_MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
 int _putchar(char c);
 void AA_itwos(int num, char *s_buff);
